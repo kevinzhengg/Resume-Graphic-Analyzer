63509efed7d45892fbcfbb3c61f9cd66f17fbbb4
kaisic1224 <hiddenshadows1224@gmail.com>
2023-09-16 08:41:22 -0400

    pdf reader

--- backend/src/scrape.py
+++ backend/src/scrape.py
@@ -1,6 +1,11 @@
 from PyPDF2 import PdfReader
 
 
-pdf_file_path = 'resume.pdf'
+# encode this so it comes from an api req, db, or something else later
+pdf_file_path = './src/resume.pdf'
 
 reader = PdfReader(pdf_file_path)
+
+page = reader.pages[0]
+
+print(page.extract_text())
